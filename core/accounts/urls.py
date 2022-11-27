82cc0265406886594e3df9e1031b2f71190b4c9e
Kiran Shivalingegowda <kiran.shivalingegowda@outlook.com>
2022-11-27 13:07:13 -0600

    mVm

--- core/accounts/urls.py
+++ core/accounts/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
 	path('register/', views.registerPage, name="register"),
 	path('login/', views.loginPage, name="login"),  
 	path('logout/', views.logoutUser, name="logout"),
+	path('product/', views.productpage, name="product"),
     path('', views.home, name="home")
-
-
 ]
