fe9cd27c8f1a60c3c5bc9a4abe842f20c5074e35
Nagaveni Rangaswamy <narjunabettahallir@unomaha.edu>
2022-12-05 01:20:59 -0600

    new ck

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -57,3 +57,14 @@ class Subdivision(models.Model):
     def __str__(self):
         return self.subdiv_name
 
+
+class UserProfile(models.Model):
+    firstname = models.CharField(max_length=51)
+    lastname = models.CharField(max_length=51)
+    address = models.CharField(max_length=101)
+    useremail = models.EmailField(max_length=70, blank=True, unique=True)
+    weburl = models.CharField(max_length=101)
+    username = models.CharField(max_length=50, null=True)
+    mobile = models.CharField(max_length=50, null=True)
+    aboutme = models.CharField(max_length=500, null=True)
+
