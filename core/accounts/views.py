80279ea38f61d84a5dbb3cc6c83e44847b57beb4
Nagaveni Rangaswamy <narjunabettahallir@unomaha.edu>
2022-12-04 20:49:49 -0600

    uno project

--- core/accounts/views.py
+++ core/accounts/views.py
@@ -26,7 +26,6 @@ def registerPage(request):
         context = {'form': form}
         return render(request, 'accounts/register.html', context)
 
-
 def loginPage(request):
     if request.user.is_authenticated:
         return redirect('home')
@@ -46,21 +45,19 @@ def loginPage(request):
         context = {}
         return render(request, 'accounts/login.html', context)
 
-
 def logoutUser(request):
     logout(request)
     return redirect('login')
-
 
 @login_required(login_url='login')
 def home(request):
     context = {}
     return render(request, 'accounts/dashboard.html', context)
 
-
 def home1(request):
     context = {}
     return render(request, 'accounts/dashboard.html', context)
+
 @login_required(login_url='login')
 def myprofile(request):
     context = {}
