48554e223a8145ca456cea1e559d2142aa44c8e7
saikirankalali <37777700+saikirankalali@users.noreply.github.com>
2022-12-10 19:04:52 -0600

    Settings- debug=false

--- core/core/settings.py
+++ core/core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k+(mz!)h2xgxpv(qp+^ikgoxf!3x_3)fissz5(-@-(24bs*o0)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
